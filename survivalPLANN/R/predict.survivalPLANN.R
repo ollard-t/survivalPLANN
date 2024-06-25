f7ddaaf4159c80b11dcd2e2dccef5657fe228371
Thomas Ollard <166803888+ollard-t@users.noreply.github.com>
2024-06-25 16:24:04 +0200

    Add files via upload

---
+++ survivalPLANN/R/predict.survivalPLANN.R
@@ -0,0 +1,61 @@
+
+predict.survivalPLANN <- function(object, newdata = NULL, newtimes = NULL, ...)
+{
+  
+  intervals = object$intervals
+  formula = object$formula
+  
+  if(!is.null(newdata))
+  {
+    if(!is.data.frame(newdata)) stop("Argument 'newdata' must be a data frame")
+    indic <- gsub("\\+", "", attr(terms(formula), "term.labels") ) %in% names(newdata) 
+    if( sum(!indic) > 0 ) stop("Missing predictor in the data frame")
+    covariates <- as.matrix(newdata[, gsub("\\+", "", attr(terms(object$formula), "term.labels"))])
+  }
+  
+  if(is.null(newdata))  { newdata <- object$data[,-dim(object$data)[2]]}
+
+
+  data_dupli = newdata[rep(seq_len(nrow(newdata)), each = length(intervals)-1), ]
+  data_dupli$Intervals <- ave(1:nrow(data_dupli),
+                              floor(as.numeric(rownames(data_dupli))),
+                              FUN = seq_along)
+  
+  
+  predictions <- predict(object$fitsurvivalnet, data_dupli, type ="raw")
+
+  grouped_df <- split(cbind(data_dupli,predictions),
+                     rep(1:ceiling(nrow(data_dupli)/(length(intervals)-1)),
+                     each=length(intervals)-1, length.out=nrow(data_dupli)))
+
+
+  result_group <- lapply(grouped_df, function(group) {
+    group$Survie <- cumprod(1-group$predictions)
+    return(group)
+  })
+  result_df <- do.call(rbind, result_group)$Survie
+
+  predictions <- as.data.frame(matrix(result_df, ncol = length(intervals)-1, byrow = TRUE))
+  predictions <- cbind(rep(1,dim(predictions)[1]), predictions)
+
+  ints_names <- c()
+
+  for( i in 1:(length(object$intervals)-1) ){
+    ints_names = c(ints_names, paste0("(",round(object$intervals[i], digit = 2), ";",
+                                      round(object$intervals[i+1], digit = 2),"]"))
+  }
+  colnames(predictions) = c("0",ints_names)
+  
+  if(is.null(newtimes))  { newtimes <- sort(unique(object$y[,1]))}
+  else{ 
+    if(!is.vector(newtimes))stop("newtimes must be a vector")
+    idx=findInterval(newtimes, intervals, left.open = TRUE)
+    predictions = predictions[,idx]
+  }
+  
+
+  res <- list(times = newtimes, predictions = predictions)
+  
+  return(res)
+  
+}
